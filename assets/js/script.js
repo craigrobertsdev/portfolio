cfed0c1ebe34d8322e5975f6f3b5686ac4851df6
Craig Roberts <craig.roberts11@outlook.com>
2023-02-28 05:43:37 +1030

    commit before trying full-screen landing page

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// const header = document.querySelector("header");
+
+// let viewportHeight = window.innerHeight;
+// const htmlPadding = document.querySelector("html").style.padding;
+// header.style.height = (viewportHeight - htmlPadding) + "px";
